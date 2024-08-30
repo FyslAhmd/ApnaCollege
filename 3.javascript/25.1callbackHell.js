99f1911d1fcc5bb245fc0b16b0ffb1e065b9d48f
FyslAhmd <info.faysal.32@gmail.com>
2024-08-30 19:57:11 +0600

    callback

---
+++ 3.javascript/25.1callbackHell.js
@@ -0,0 +1,29 @@
+let h1 = document.querySelector("h1");
+
+function changeColor(color,delay,nextColor) {
+    setTimeout(function() {
+        h1.style.color = color;
+        if(nextColor) nextColor();
+    },delay);
+}
+
+changeColor("red",1000,()=> {
+    changeColor("green",1000,()=> {
+        changeColor("yellow",1000,()=> {
+            changeColor("blue",1000);
+        });
+    });
+});
+
+function saveDB(data) {
+    return new Promise((resolve,reject) => {
+        let intSpeed = Math.floor(Math.random()*10+1);
+        if(intSpeed>4) {
+            resolve("Success");
+        }
+        else {
+            reject("Failure");
+        }
+    });
+}
+saveDB("Faysal Ahmed");
