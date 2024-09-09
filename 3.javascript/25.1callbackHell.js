b6b7ba81583a86288ba06ce24f4454c10c8e72ef
FyslAhmd <info.faysal.32@gmail.com>
2024-09-10 01:19:10 +0600

    js updated

--- 3.javascript/25.1callbackHell.js
+++ 3.javascript/25.1callbackHell.js
@@ -6,7 +6,6 @@ function changeColor(color,delay,nextColor) {
         if(nextColor) nextColor();
     },delay);
 }
-
 changeColor("red",1000,()=> {
     changeColor("green",1000,()=> {
         changeColor("yellow",1000,()=> {
